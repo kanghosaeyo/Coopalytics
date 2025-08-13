dc916b30a213a987e5e340db92e9e61b47ebbc53
aga1936 <antic.a@northeastern.edu>
2025-08-13 00:11:13 -0400

    change position preference

---
+++ api/backend/viewsPos/viewsPos_routes.py
@@ -0,0 +1,37 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+# New Blueprint for applications
+views_position = Blueprint('views_position', __name__)
+
+
+@views_position.route('/views_position', methods=['POST'])
+def set_job_preference():
+    the_data = request.json
+    current_app.logger.info(the_data)
+    
+    student_id = the_data['studentId']
+    coop_position_id = the_data['coopPositionId']
+    preference = the_data['preference']  
+    
+    query = f'''
+        INSERT INTO viewsPos (studentId, coopPositionId, preference)
+        VALUES ({student_id}, {coop_position_id}, {int(preference)})
+        ON DUPLICATE KEY UPDATE preference = VALUES(preference)
+    '''
+    current_app.logger.info(query)
+    
+    # Execute query and commit
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    # Return success response
+    response = make_response("Preference saved successfully")
+    response.status_code = 200
+    return response
+
