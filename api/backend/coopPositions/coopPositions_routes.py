55fbddc680afe6320aa7d8464a63877fa76fb77d
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-08-14 21:32:29 -0400

    updating DEI metrics (again)

--- api/backend/coopPositions/coopPositions_routes.py
+++ api/backend/coopPositions/coopPositions_routes.py
@@ -26,7 +26,7 @@ def get_position_info():
 
 
 # Student/Advisor views the average pay for each industry
-@coopPositions.route('/industryAveragePay', methods=['GET'])
+@coopPositions.route('/coopPositions/industryAveragePay', methods=['GET'])
 def get_industry_average_pay():
     query = '''
         SELECT cp.industry, AVG(cp.hourlyPay) AS industryAvgHourlyPay
@@ -138,7 +138,7 @@ def create_position():
 
 
 # Admin reviews positions before they go live 
-@coopPositions.route('/pending', methods=['GET'])
+@coopPositions.route('/coopPositions/pending', methods=['GET'])
 def get_pending_positions():
     current_app.logger.info('GET /pending route')
 
@@ -171,7 +171,7 @@ def get_pending_positions():
     return the_response
 
 # Admin views number of co-ops posted by each employer
-@coopPositions.route('/employerJobCounts', methods=['GET'])
+@coopPositions.route('/coopPositions/employerJobCounts', methods=['GET'])
 def get_employer_job_counts():
     current_app.logger.info('GET /employerJobCounts route')
 
